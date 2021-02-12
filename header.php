5a5cf6445a54b153e2b9aec30d956358be65c591
Tomy-my <azerouto@gmail.com>
2021-02-12 16:57:30 +0100

    deuxième commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/fonts.css" />
+        <link rel="stylesheet" href="css/styles.css" />
+        <link rel="icon" href="images/favicon.png" />
+        <title>Portfolio : <?php echo $title; ?></title>
+    </head>
+        <body>
+        <div class="navbar">
+            <div id="logo">   Portfolio  </div>
+            <a href="index.php" id="home"> Home </a>
+            <a href="#" id="about"> About me </a>
+            <a href="#" id="project"> Projet </a>
+            <a href="#" id="cv"> C.V </a>
+            <a href="#" id="contact"> Contact </a>
+        </div>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php 
+    include('header.php')
+?>
+
+
+<style>
+    body {
+        background-image: url('./images/01000646.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-attachment: fixed;
+    }
+</style>
+
+
+<div class="welcome">
+    <div class="titre"> 
+        <h2>Welcome </h2>
+        <h4> To My </h4>
+        <h1> Portfolio </h1>
+    </div>
+    <div class="description">
+        <p>
+        Vous trouverez grâce à l'aide du menu ainsi qu'aux deux boutons si dessous, toutes les informations importantes me concernant. Projet web, C.V., etc..        
+        </p>
+    </div>
+        <button type="text" class="redirection1" onclick="window.location.href = '#';">About-me</button>
+        <button type="text" class="redirection2" onclick="window.location.href = '#';">Me contacter</button>
+</div>
+
+
+
+
+
+
+
+
+
+
+<?php
+    include('footer.php')
+?>
