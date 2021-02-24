b6466579776f16b9eb36ec15f5c0a077fba9dc3b
Tomy-my <azerouto@gmail.com>
2021-02-24 14:42:48 +0100

    ~

--- cv.php
+++ cv.php
@@ -62,39 +62,72 @@
             <div class="coordonne">
                 <h1> Tomy SOTTY</h1>
                 <h3>Tomy.vdtlj<span>@</span>gmail.com</h3>
-                <div class="permis"> </div>
+                <div class="permis">
+                    <div class="info_permis"> Permis B <span>✓</span></div>
+                    <div class="image_permis"> </div>
+                </div>
                 <div class="ecole"> </div>
             </div>
             <div class="contact1-pic js-tilt" data-tilt>
 				<img src="./images/projet.png" alt="IMG">
 			</div>
             <div class="qualites">
-                <h1>Qualités</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                In laboriosam, velit nam aut repellat nobis alias voluptatum 
-                expedita! Quaerat debitis dolores, accusamus atque tenetur 
-                voluptates 
-                reprehenderit repudiandae est labore recusandae.
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                In laboriosam, velit nam aut repellat nobis alias voluptatum 
-                expedita! Quaerat debitis dolores, accusamus atque tenetur 
-                voluptates 
-                reprehenderit repudiandae est labore recusandae.
-                </p>
+                <div class="qualites_1">
+                    <h1>Qualités</h1>
+                    <h3>persévérance :</h3>
+                    <div class="perseverance">
+                        <div class="slide_perseverance"> </div>
+                    </div>
+                    <h3>Curiosité :</h3>
+                    <div class="curiosite">
+                        <div class="slide_cursiosite"> </div>
+                    </div>
+                    <h3>Minutieux :</h3>
+                    <div class="minutieux">
+                        <div class="slide_minutieux"> </div>
+                    </div>
+                </div>
+                <div class="qualites_2">
+                    <h3>Logique :</h3>
+                    <div class="logique">
+                        <div class="slide_logique"> </div>
+                    </div>
+                    <h3>Autonomme :</h3>
+                    <div class="autonomme">
+                        <div class="slide_autonomme"> </div>
+                    </div>
+                    <h3>À l'écoute :</h3>
+                    <div class="ecoute">
+                        <div class="slide_ecoute"> </div>
+                    </div>
+                </div>
             </div>
             <div class="competences">
+                <div class="competences_1">
                 <h1>Competences</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                In laboriosam, velit nam aut repellat nobis alias voluptatum 
-                expedita! Quaerat debitis dolores, accusamus atque tenetur 
-                voluptates 
-                reprehenderit repudiandae est labore recusandae.
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                In laboriosam, velit nam aut repellat nobis alias voluptatum 
-                expedita! Quaerat debitis dolores, accusamus atque tenetur 
-                voluptates 
-                reprehenderit repudiandae est labore recusandae.
-                </p>
+                    <h3>HTML :</h3>
+                    <div class="html">
+                        <div class="slide_html"> </div>
+                    </div>
+                    <h3>CSS :</h3>
+                    <div class="css">
+                        <div class="slide_css"> </div>
+                    </div>
+                    <h3>JavaScript :</h3>
+                    <div class="js">
+                        <div class="slide_js"> </div>
+                    </div>
+                </div>
+                <div class="competences_2">
+                    <h3>PHP :</h3>
+                    <div class="php">
+                        <div class="slide_php"> </div>
+                    </div>
+                    <h3>Word :</h3>
+                    <div class="word">
+                        <div class="slide_word"> </div>
+                    </div>
+                </div>
             </div>
         </div>
 
